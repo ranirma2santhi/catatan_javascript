f1c419da3d39066d758ec74f41bb1de216764e08
Ni Made Rai Nirmala Santhi <65942352+ranirma2santhi@users.noreply.github.com>
2022-06-15 23:37:35 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("Hello World!")
+
+//komentar
+/*
+  ini
+  komentar
+  ya!
+*/
+
+let lastName;
+lastName = "Rai Santhi";
+
+console.log(lastName);
+
+// DATA TYPE
+
+// 1. Undefined
+let dataType1;
+console.log(typeof(x));
+
+// 2. Numbers
+let dataType2 = 18;
+console.log(typeof(dataType2))
+
+let dataType2_2 = 18.0601;
+console.log(typeof(dataType2_2));
+
+// 3. BigIn
+const dataType3 = 180820011912200729121998311219761103197322032022130820042601200811102011;
+console.log(dataType3);
+
+// 4. String
+let dataType4 = "Hi Santhi";
+console.log(typeof(dataType4));
+
+// 5. Boolean 
+let dataType5_1 = true;
+let dataType5_2 = false;
+
+console.log(typeof(dataType5_1));
+console.log(typeof(dataType5_2));
